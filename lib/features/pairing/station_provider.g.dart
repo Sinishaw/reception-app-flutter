c874826671fc23186f77df174ee3e73f61b54dea
Sinishaw Kassa <sinishawkassa@gmail.com>
2026-05-18 15:00:23 +0300

    Add session termination button to settings and autocomplete floor selection from mock API #18

--- lib/features/pairing/station_provider.g.dart
+++ lib/features/pairing/station_provider.g.dart
@@ -22,5 +22,21 @@ final stationIdProvider =
     );
 
 typedef _$StationId = AutoDisposeNotifier<String?>;
+String _$assignedFloorHash() => r'6a570e6c84ae916dee0c34f09d27647cf1ab1c98';
+
+/// See also [AssignedFloor].
+@ProviderFor(AssignedFloor)
+final assignedFloorProvider =
+    AutoDisposeNotifierProvider<AssignedFloor, String?>.internal(
+      AssignedFloor.new,
+      name: r'assignedFloorProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$assignedFloorHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$AssignedFloor = AutoDisposeNotifier<String?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
